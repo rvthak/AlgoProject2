6b5ce31643b025846fce785737eb7fcd9d9a5f25
ChristosTsouchlaris <ctsouch@outlook.com>
2021-12-07 13:10:00 +0200

    Fixed bug in utils

--- include/utils.h
+++ include/utils.h
@@ -43,10 +43,7 @@ bool positive_less_than(double x, double y);
 
 // CHRIS 07.12.2021 START
 
-double distance_between_points(double x1, double y1, double x2, double y2)
-{
-    return (double) sqrt(pow(x2 - x1, 2) + pow(y2 - y1, 2) * 1.0);
-}
+double distance_between_points(double x1, double y1, double x2, double y2);
 
 // CHRIS 07.12.2021 END
 
