57ce763f785435f2a4e590b87d72edc746cbef56
Christos Tsouchlaris <75982015+ChristosTsouchlaris@users.noreply.github.com>
2021-12-17 11:56:30 +0200

    Cleaned comments

--- include/DistanceUnitTest.h
+++ include/DistanceUnitTest.h
@@ -43,7 +43,6 @@ private:
 
 public:
     static CppUnit::Test* suite();
-    // void setUp();
     void test_discrete_distance_1_1();
     void test_discrete_distance_1_2();
     void test_discrete_distance_2_1();
