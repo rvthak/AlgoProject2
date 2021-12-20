b1c90159ffc82a2b08a732e7c5415adc3cd91fae
Giannis Rovithakis <giannis.rovithakis@gmail.com>
2021-12-20 23:01:15 +0200

    Added the necessary functions to support the Complete tree struct

--- include/VCurve.h
+++ include/VCurve.h
@@ -4,32 +4,47 @@
 #include <vector>
 #include "Vector.h"
 
+// A small constant used as the limit for filtering
+#define E_CONSTANT 0.3
+
 // Represents a Vector as a Curve considering that sampling rate (x-axis values) is 1 unit
 struct VCurve{
 	Vector *vec;
 	std::vector<double> x, y;
 	unsigned max_vec_size;
 	
+	VCurve();
 	VCurve(Vector *v);
+	VCurve(std::vector<double> *v);
 
 	// VCurve Mutators
-	void filter();				// Filter-out consecutive points that show very little volatility
-	void minima_maxima();		// Filter-out non-important points, keep only local minima and maxima
-	void remove_duplicates();	// Remove any duplicate points that may result from grid snapping
+	void filter(double e = E_CONSTANT);	// Filter-out consecutive points that show very little volatility
+	void minima_maxima();				// Filter-out non-important points, keep only local minima and maxima
+	void remove_duplicates();			// Remove any duplicate points that may result from grid snapping
+	void add_padding();					// Add the needed amout of padding to get the curve to "max_vec_size"
 
 	// VCurve Grid Snapping
 	void snap_2D(double delta, double t);
 	void snap_1D(double delta, double t);
 
-	// Utils
-	void add_padding();		// Add the needed amout of padding to get the curve to "max_vec_size"
 	Vector *vectorize();	// Converts the VCurve to a Vector and returns a pointer to it
-	unsigned size();		// Returns the current VCurve length
-	void print();			// Prints the VCurve
+	std::vector<double> vectorize_dim(unsigned dim);
+	std::vector<double> vectorize_dim(unsigned dim, unsigned limit);
+	std::vector<double> vectorize_fill(unsigned dim, unsigned size);
+	std::vector<double> vectorize_size(unsigned size);
+
+	// Utils
+	unsigned size();	// Returns the current VCurve length
+	void print();		// Prints the VCurve
+	bool isEmpty();		// Returns true if the Curve is currently empty
+	void push_back(double xi, double yi);	// Add the given point to the Curve
+	void set_max_size(unsigned size);		// Setter for 'max_vec_size'
 };
 
+// Return the discrete frechet mean curve of the two given curves
+VCurve *meanCurve(VCurve *p, VCurve *q);
+
 // We work under the assumption that all the Vectors of our dataset share the same length
-// "max_vec_size": It corresponds to the Vector complexity * 2 
 
 // A.ii  Pipeline: snap_2D -> remove_duplicates -> padding 
 // A.iii Pipeline: filter -> snap_1D -> minima_maxima -> padding 
--- src/VCurve.cpp
+++ src/VCurve.cpp
@@ -7,8 +7,19 @@ using namespace std;
 // A Large prime + a decimal part - just because I like primes
 #define PADDING_NUM 1000003.700171
 
-// A small constant used as the limit for filtering
-#define E_CONSTANT 0.3
+#define LEFT_MOVE 0
+#define DIAG_MOVE 1
+#define DOWN_MOVE 2
+
+unsigned max_move(double left, double diag, double down);
+double bounds(double **c, int pi, int qi);
+
+//------------------------------------------------------------------------------------------------------------------
+
+VCurve::VCurve(){
+	this->vec = nullptr;
+	this->max_vec_size = 0;
+}
 
 // Convert the given Vector to a VCurve
 // Uses sampling rate = 1 unit of time
@@ -19,6 +30,15 @@ VCurve::VCurve(Vector *v){
 		(this->y).push_back( (v->vec)[i] );
 	}
 	this->vec = v;
+	this->max_vec_size = v->size();
+}
+
+VCurve::VCurve(std::vector<double> *v){
+	for(unsigned i=0; i<(v->size()); i++){
+		(this->x).push_back(i);
+		(this->y).push_back( (*v)[i] );
+	}
+	this->vec = nullptr;
 	this->max_vec_size = v->size();
 }
 
@@ -34,12 +54,77 @@ Vector *VCurve::vectorize(){
 	}
 
 	//cout << vec->size() << endl;
-	
 	return vec;
 }
 
+// Convert only the requested dimention into a vector
+std::vector<double> VCurve::vectorize_dim(unsigned dim){
+	std::vector<double> v, *ptr;
+
+	if( dim == 0 ){ ptr = &(this->x); }
+	else{ ptr = &(this->y); }
+
+	for(unsigned i=0; i<(this->size()); i++){
+		v.push_back( (*ptr)[i] );
+	}
+	return v; 
+}
+
+// Convert only the requested dimention into a vector
+// Stop the conversion and return if the given limit length was surpassed
+std::vector<double> VCurve::vectorize_dim(unsigned dim, unsigned limit){
+	std::vector<double> v, *ptr;
+
+	if( dim == 0 ){ ptr = &(this->x); }
+	else{ ptr = &(this->y); }
+
+	for(unsigned i=0; i<(this->size()); i++){
+		v.push_back( (*ptr)[i] );
+		if( v.size() == limit ){ break; }
+	}
+	return v; 
+}
+
+// Convert only the requested dimention into a vector
+// Fill the end of the vector with the last element
+std::vector<double> VCurve::vectorize_fill(unsigned dim, unsigned size){
+	std::vector<double> v, *ptr;
+
+	if( dim == 0 ){ ptr = &(this->x); }
+	else{ ptr = &(this->y); }
+
+	for(unsigned i=0; i<(this->size()); i++){
+		v.push_back( (*ptr)[i] );
+	}
+
+	for(unsigned i=(this->size()); i<size; i++){
+		v.push_back( v[(this->size())-1] );
+	}
+	return v; 
+}
+
+// Convert the Curve to a vector of the given size,
+// try to not lose too much information
+// We know that any given curve cannot be smaller than the given size, so we
+// just need to cut/filter it down to the required size somehow
+std::vector<double> VCurve::vectorize_size(unsigned size){
+
+	// If we are lucky and we already have the correct size, just return it as it is
+	if( (this->size()) == size ){ return vectorize_dim(1); }
+
+	// Else we will need to shorted the curve length
+
+	// Try filtering
+	this->filter(0.5);
+	if( (this->size()) == size ){ return vectorize_dim(1); }
+	if( (this->size()) < size ){ return vectorize_fill(1,size); }
+
+	// Else just cut the end off
+	return vectorize_dim(1, size);
+}
+
 // Filter-out consecutive points that show very little volatility
-void VCurve::filter(){
+void VCurve::filter(double e){
 	// The filtered vectors
 	vector<double> x, y;
 
@@ -50,8 +135,8 @@ void VCurve::filter(){
 	// Iterate through the VCurve triplets and filter-out any insignificant values
 	for(unsigned i=1; i<((this->size())-1); i++){
 
-		if( abs( (this->y)[i-1] - (this->y)[i] ) <= E_CONSTANT &&
-		    abs( (this->y)[i+1] - (this->y)[i] ) <= E_CONSTANT ){
+		if( abs( (this->y)[i-1] - (this->y)[i] ) <= e &&
+		    abs( (this->y)[i+1] - (this->y)[i] ) <= e ){
 			continue;
 		}
 		x.push_back( (this->x)[i] );
@@ -154,4 +239,106 @@ void VCurve::print(){
 		cout << "(" << (this->x)[i] << "," << (this->y)[i] << ") ";
 	}
 	cout << "]" << endl;
+}
+
+void VCurve::set_max_size(unsigned size){ this->max_vec_size = size; }
+
+// Returns true if the Curve is currently empty
+bool VCurve::isEmpty(){ return ((this->max_vec_size) == 0); }
+
+// Add the given point to the Curve
+void VCurve::push_back(double xi, double yi){
+	(this->x).push_back(xi);
+	(this->y).push_back(yi);
+}
+
+//------------------------------------------------------------------------------------------------------------------
+
+// Calculate the Mean Discrete Frechet curve of the two given Curves
+VCurve *meanCurve(VCurve *p, VCurve *q){
+
+	// In case only one curve was given => return it as the mean
+	if( (q->size()) == 0 ){ return p; }
+
+	// Start by calculating the discrete frechet distance between the two vectors
+	// Exact similar process with Vector::discrete_frechet_distance()
+	double **c    = new_2D_table(p->size(), q->size());
+	double **dist = new_2D_table(p->size(), q->size());
+
+	// Calculate the distances and store them
+	for(unsigned i=0; i<(p->size()); i++){	// For every 2D point in Vector 'p'
+		for(unsigned j=0; j<(q->size()); j++){	// For every 2D point in Vector 'q'
+			dist[i][j] = euclidian_distance((p->x)[i],(p->y)[i], (q->x)[j],(q->y)[j]);
+		}
+	}
+
+	// Use the distances to calculate the Frechet distance
+	for(unsigned i=0; i<(p->size()); i++){
+		for(unsigned j=0; j<(p->size()); j++){
+			if( (i==0)&&(j==0) ){
+				c[i][j] = dist[i][j];
+			} else if( i==0 ){
+				c[i][j] = max( c[i][j-1], dist[i][j] );
+			} else if( j==0 ){
+				c[i][j] = max( c[i-1][j], dist[i][j] );
+			} else {
+				c[i][j] = max( min( min( c[i-1][j],c[i-1][j-1] ), c[i][j-1] ), dist[i][j] );
+			}
+		}
+	}
+
+	// Now we can backtrack the table to get an optimal traversal
+	std::vector<int> trav_p, trav_q;
+
+	// We start from the solution (the final cell of the table)
+	// and work our way back to the first cell, while storing the steps
+	int pi=(p->size())-1, qi=(q->size())-1, move;
+
+	trav_p.push_back(pi);
+	trav_q.push_back(qi);
+	
+	while( pi!=0 && qi!=0 ){
+		move = max_move( bounds(c,pi-1,qi), bounds(c,pi-1,qi-1), bounds(c,pi,qi-1) );
+
+		// Store the Curve indexes per traversal step
+		if( move == LEFT_MOVE ){ // left
+			pi--;
+		}
+		else if( move == DIAG_MOVE ){ // diag
+			pi--; qi--;
+		}
+		else{ // down == DOWN_MOVE
+			qi--;
+		}
+		trav_p.push_back(pi);
+		trav_q.push_back(qi);
+	}
+
+	// Free the tables
+	delete_2D_table(c, p->size()); delete_2D_table(dist, p->size());
+	
+	// Based on the resulting traversal => average the step points to the mean vector
+	VCurve *mean = new VCurve();
+
+	for(unsigned i=0; i<(trav_p.size()); i++){
+		mean->push_back( average( trav_p[i], trav_q[i] ), 
+			            average( (p->y)[trav_p[i]], (q->y)[trav_q[i]] ));
+	}
+
+	mean->set_max_size(trav_p.size());
+	return mean;
+}
+
+// Return a code that indicates which direction has the largest value
+unsigned max_move(double left, double diag, double down){
+	if( (left >= diag) && (left >= down) ){ return LEFT_MOVE; }
+	if( (diag >= left) && (diag >= down) ){ return DIAG_MOVE; }
+	else{ return DOWN_MOVE; }
+}
+
+// Ensure that the given index is inside table and then return it
+// if it is not in table bounds, return zero
+double bounds(double **c, int pi, int qi){
+	if( (pi < 0) || (qi < 0) ){ return 0; }
+	return c[pi][qi];
 }
